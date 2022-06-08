0ae8fa967c787d7931965d7f6c3d3ce21c42eacd
ndnghiadn <ndnghiadn@gmail.com>
2022-06-08 12:37:15 +0700

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = ({ className }) => {
         <ul>
             {
               items.map((item, index) => (
-                <li key={index} className={`navbar--list ${item.link === router.asPath && 'active'}`}>
-                  <a href={item.link}>
+                <li key={index} className={`navbar--list ${item.link === router.asPath && 'active'}`} onClick={() => router.push(item.link)}>
+                  <a>
                     <span className="navbar--list_icon"><ion-icon name={item.icon}></ion-icon></span>
                     <span className="navbar--list_text">{item.name}</span>
                   </a>
